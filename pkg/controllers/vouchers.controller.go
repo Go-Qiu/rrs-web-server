9722105441ed7e99ddafa6a4a1f819c17529c042
Dylan Kiew <das.dylan.qiu@gmail.com>
2022-06-18 10:46:23 +0800

    basic code for Vouchers API

--- pkg/controllers/vouchers.controller.go
+++ pkg/controllers/vouchers.controller.go
@@ -2,12 +2,27 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type VouchersCtl struct {
 	name   string
 	apikey string
+}
+
+type qty struct {
+	dollars10 int
+	dollars5  int
+	dollars2  int
+	dollars1  int
+}
+type InConvertPointsToVouchers struct {
+	userId   string
+	points   int
+	vouchers struct {
+		qty qty
+	}
 }
 
 func NewVouchersCtl(name string, apikey string) *VouchersCtl {
@@ -33,4 +48,82 @@ func (v *VouchersCtl) GetAll(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(respBody))
 	//
+}
+
+// GetByIdentifier retrieves a specific voucher data that matches the unique data id assigned at its creation.
+func (v *VouchersCtl) GetById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// data in json string format
+	data := ""
+
+	respBody := fmt.Sprintf(`{
+		"ok" : true,
+		"msg" : "[MS-VOUCHERS]: retrieval voucher data, successful",
+		"data" : {%s}
+	}`, data)
+
+	// send response to the requesting device
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(respBody))
+	//
+}
+
+// RedeemById updates a specific voucher data (identified by Id) and change its status to 'REDEEMED'.
+func (v *VouchersCtl) RedeemById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// data in json string format
+	data := ""
+
+	respBody := fmt.Sprintf(`{
+		"ok" : true,
+		"msg" : "[MS-VOUCHERS]: redeemed voucher data, successful",
+		"data" : {%s}
+	}`, data)
+
+	// send response to the requesting device
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(respBody))
+	//
+}
+
+func (v *VouchersCtl) HandlePostRequest(w http.ResponseWriter, r *http.Request) {
+
+	// need to replace with code that reads the action value from the request body.
+
+	action := "CONVERT-POINTS-TO-VOUCHERS"
+	inputs := InConvertPointsToVouchers{}
+	w.Header().Set("Content-Type", "application/json")
+
+	// data in json string format
+	data := ""
+
+	if action == "CONVET-POINT-TO-VOUCHERS" {
+		err := convertPointsToVouchers(inputs)
+		if err != nil {
+			// error handling code (to be done)
+			log.Println(err)
+		}
+	}
+
+	// ok.
+	respBody := fmt.Sprintf(`{
+		"ok" : true,
+		"msg" : "[MS-VOUCHERS]: converted points to vouchers, successful",
+		"data" : {%s}
+	}`, data)
+
+	// send response to the requesting device
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(respBody))
+	//
+}
+
+// convertPointsToVouchers will convert a specific number of reward points, for a specific user id, to a specific set of vouchers (different value quantum).
+func convertPointsToVouchers(in InConvertPointsToVouchers) error {
+
+	// call api endpoint at vouher system (to be done)
+
+	return nil
 }
--- pkg/http/routers/vouchers.router.go
+++ pkg/http/routers/vouchers.router.go
@@ -7,4 +7,8 @@ import (
 
 func RegisterVouchersRouter(router *mux.Router, ctl *controllers.VouchersCtl) {
 	router.HandleFunc("/", ctl.GetAll).Methods("GET")
+	router.HandleFunc("/", ctl.HandlePostRequest).Methods("POST")
+	router.HandleFunc("/{id}", ctl.GetById).Methods("GET")
+	router.HandleFunc("/{id}", ctl.RedeemById).Methods("PUT")
+
 }
