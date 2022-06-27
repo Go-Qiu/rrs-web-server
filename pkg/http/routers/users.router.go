cd6aeed371d5363d124b13fa28f080950d468e6d
Dylan Kiew <das.dylan.qiu@gmail.com>
2022-06-27 15:01:46 +0800

    apply middleware to validate token

--- pkg/http/routers/users.router.go
+++ pkg/http/routers/users.router.go
@@ -2,12 +2,13 @@ package routers
 
 import (
 	"github.com/go-qiu/rrs-web-server/pkg/controllers"
+	"github.com/go-qiu/rrs-web-server/pkg/middlewares"
 	"github.com/gorilla/mux"
 )
 
 func RegisterUsersRouter(router *mux.Router, ctl *controllers.UserCtl) {
 	// router.HandleFunc("/{id}/transactions/type/{type_code}", ctl.GetTransactionsByType).Methods("GET")
-	router.HandleFunc("/{id}/transactions", ctl.GetTransactionsByType).Methods("GET").Queries("type", "{type_code}")
-	router.HandleFunc("/{id}/points", ctl.GetPoints).Methods("GET")
-	router.HandleFunc("/{id}/voucers", ctl.GetPoints).Methods("GET")
+	router.HandleFunc("/{id}/transactions", middlewares.ValidateToken(ctl.GetTransactionsByType)).Methods("GET").Queries("type", "{type_code}")
+	router.HandleFunc("/{id}/points", middlewares.ValidateToken(ctl.GetPoints)).Methods("GET")
+	router.HandleFunc("/{id}/voucers", ctl.GetVouchers).Methods("GET")
 }
